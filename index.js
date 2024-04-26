3a9dca6d2c8a9c81c7c51803a5070d18f5b684a8
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-04-26 20:43:04 +0530

    code added

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ app.get("/ig/:username", (req, res) => {
     let { username } = req.params;
     const instaData = require("./data.json");
     const data = instaData[username];
+    console.log(data);
     res.render("instagram.ejs", { data });
 });
 
